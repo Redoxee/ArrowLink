a7915c5da1a7a46645fc3085d7798cdfdf24949b
Redoxee <anton.francis.roy@gmail.com>
2017-10-14 12:51:07 +0200

    starting particle refacto

---
+++ ArrowLink/Assets/Scripts/Misc/LinkParticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArrowLink
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class LinkParticle : MonoBehaviour
+    {
+        ParticleSystem particle;
+        private void Start()
+        {
+            particle = transform.GetComponent<ParticleSystem>();
+            var mainParameters = particle.main;
+            mainParameters.startRotationZ = Mathf.Deg2Rad * 45;
+            mainParameters.startSizeY = 2;
+            var colors = particle.colorOverLifetime;
+        }
+    }
+}
