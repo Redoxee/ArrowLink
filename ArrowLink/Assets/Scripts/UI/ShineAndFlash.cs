c2a157225cbefc6bda6f422530dee99045072e1e
Redoxee <anton.francis.roy@gmail.com>
2018-06-08 11:37:50 +0200

    More particles

--- ArrowLink/Assets/Scripts/UI/ShineAndFlash.cs
+++ ArrowLink/Assets/Scripts/UI/ShineAndFlash.cs
@@ -10,6 +10,8 @@ public class ShineAndFlash : MonoBehaviour {
     ParticleSystem m_secondLevel = null;
     [SerializeField]
     ParticleSystem m_thirdLevel = null;
+    [SerializeField]
+    ParticleSystem m_fourthLevel = null;
 
     [SerializeField]
     private int m_firstThreshold = 0;
@@ -17,8 +19,11 @@ public class ShineAndFlash : MonoBehaviour {
     private int m_secondThreshold = 0;
     [SerializeField]
     private int m_thirdThreshold = 0;
+    [SerializeField]
+    private int m_fourthThreshold = 0;
 
     private bool m_shouldBoom = false;
+    private bool m_shouldFourth = false;
 
     public void StartShine(int value)
     {
@@ -32,13 +37,20 @@ public class ShineAndFlash : MonoBehaviour {
         }
 
         m_shouldBoom = value >= m_secondThreshold;
+        m_shouldFourth = value > m_fourthThreshold;
     }
 
     public void StopShine()
     {
-        if (m_firstLevel.isEmitting && m_shouldBoom)
-        {
-            m_secondLevel.Play();
+        if (m_firstLevel.isEmitting)
+        {    if (m_shouldBoom)
+            {
+                m_secondLevel.Play();
+            }
+            if (m_shouldFourth)
+            {
+                m_fourthLevel.Play();
+            }
         }
         m_firstLevel.Stop();
         m_thirdLevel.Stop();
